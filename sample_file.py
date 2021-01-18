5a90d7b36f71c731784bbe27f0eda4f563dc0c27
Grzegorz Bokota <bokota+github@gmail.com>
2021-01-18 14:44:43 +0100

    sample changes branch 4

--- sample_file.py
+++ sample_file.py
@@ -1,6 +1,16 @@
+import sys
+
+
 def function1():
-    print("call of function 1")
+    """docstring for function 1"""
+    print("Call of function 1")
 
 
 def function2():
-    print("call of function 2")
+    """docstring for function 2"""
+    print("Call of function 2")
+
+
+def function3():
+    """docstring for function 3"""
+    print("Call of function 3", file=sys.stderr)
